875b6fd62a18d1678e81203f0a04bcc28b2226af
Khaya Ngwenya <khaya.developer@gmail.com>
2023-03-22 21:07:31 +0200

    Update show-article-component.blade.php

--- resources/views/livewire/article/show-article-component.blade.php
+++ resources/views/livewire/article/show-article-component.blade.php
@@ -1,5 +1,13 @@
 <div>
-    <header class="masthead" style="background-image: url({{ $article->article_image }})">
+    @php
+    if (str_starts_with($article->article_image, 'https')) {
+        $image_url = $article->article_image;
+    }else {
+        $image_url = '/storage/'.$article->article_image;
+    }
+
+    @endphp
+    <header class="masthead" style="background-image: url({{ $image_url }})">
         <div class="container position-relative px-4 px-lg-5">
             <div class="row gx-4 gx-lg-5 justify-content-center">
                 <div class="col-md-10 col-lg-8 col-xl-7">
